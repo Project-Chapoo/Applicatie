ed6f3cb8ea1052f228435b72833c3b931b4b9a26
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-06-04 15:40:54 +0200

    SendBillItems not working yet

--- ChapeauApplicatie/ChapeauDAL/PayingDAO.cs
+++ ChapeauApplicatie/ChapeauDAL/PayingDAO.cs
@@ -10,7 +10,7 @@ namespace ChapeauDAL
         public Bill GetOrderInfo(int orderID)
         {
             string query = "SELECT * FROM[dbo].[Order] AS O JOIN[dbo].[Tables] AS T ON O.TableID = T.TableID JOIN[dbo].[Employee] AS E ON T.EmployeeID = E.EmployeeID WHERE O.OrderID = @orderID";
-                            
+
             SqlParameter[] sqlParameters = new SqlParameter[1];
             sqlParameters[0] = new SqlParameter("@orderID", SqlDbType.Int) { Value = orderID };
             return ReadTablesGetOrderInfo(ExecuteSelectQuery(query, sqlParameters));
@@ -45,7 +45,7 @@ namespace ChapeauDAL
         {
             List<BillItem> billItems = new List<BillItem>();
             int currentItemID = 1;
-            foreach(DataRow dr in dataTable.Rows)
+            foreach (DataRow dr in dataTable.Rows)
             {
                 BillItem billItem = new BillItem();
                 billItem.BillItemID = currentItemID;
@@ -61,10 +61,8 @@ namespace ChapeauDAL
 
         public void SendBill(Bill bill)
         {
-            string query = "INSERT INTO [dbo].[Receipt] ([ReservationID], [TableID], [commentary], [DateAndTime], [EmployeeFirstName], [EmployeeLastName]) VALUES (@ReservationID, @TableID, @commentary, @DateAndTime, @EmployeeFirstName, @EmployeeLastName)";
-            // Sql parameters with ReservationID, TableID, commentary, DateAndTime, EmployeeFirstName, EmployeeLastName
+            string query = "INSERT INTO [dbo].[Receipt] ([TableID], [commentary], [DateAndTime], [EmployeeFirstName], [EmployeeLastName]) VALUES (@TableID, @commentary, @DateAndTime, @EmployeeFirstName, @EmployeeLastName)";
             SqlParameter[] sqlParameters = {
-                new SqlParameter("@ReservationID", SqlDbType.Int) { Value = bill.OrderID },
                 new SqlParameter("@TableID", SqlDbType.Int) { Value = bill.TableID },
                 new SqlParameter("@commentary", SqlDbType.VarChar) { Value = bill.Comment },
                 new SqlParameter("@DateAndTime", SqlDbType.DateTime) { Value = DateTime.Now },
@@ -72,6 +70,21 @@ namespace ChapeauDAL
                 new SqlParameter("@EmployeeLastName", SqlDbType.VarChar) { Value = bill.EmployeeLastName }
             };
             ExecuteEditQuery(query, sqlParameters);
+        }
+
+        public void SendBillItems(Bill bill)
+        {
+            string query = "INSERT INTO [dbo].[ReceiptItem] ([ReceiptID], [MenuItemID], [Quantity]) VALUES (@ReceiptID, @MenuItemID, @Quantity)";
+            // Sql parameters with ReceiptID, MenuItemID, Quantity
+            foreach (BillItem billItem in bill.billItems)
+            {
+                SqlParameter[] sqlParameters = {
+                new SqlParameter("@ReceiptID", SqlDbType.Int) { Value = bill.ReceiptID },
+                new SqlParameter("@MenuItemID", SqlDbType.Int) { Value = billItem.MenuItemID },
+                new SqlParameter("@Quantity", SqlDbType.Int) { Value = billItem.Quantity }
+            };
+            ExecuteEditQuery(query, sqlParameters);
+            }
         }
     }
 }
