5868ab14b5593d4b65a3710fce68c791d916a81c
l-huisman <64755533+l-huisman@users.noreply.github.com>
2022-06-11 13:26:33 +0200

    Altered the startup config to main PDA startup

--- ChapeauApplicatie/ChapeauUI/Program.cs
+++ ChapeauApplicatie/ChapeauUI/Program.cs
@@ -13,7 +13,7 @@ namespace ChapeauUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Payingfrm(1));
+            Application.Run(new PDAOrdering_Joey());
         }
     }
 }
